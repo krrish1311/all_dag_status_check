3617d334a53cab9f98ca6830d5c7039e33063bd4
krishna Digole <krrishdigole@gmail.com>
2023-05-15 16:52:39 +0530

    The DAG's status are checked and accordingly the Mail has been send

--- all_dag_run_fail_status.py
+++ all_dag_run_fail_status.py
@@ -1,6 +1,6 @@
 
 import smtplib
-from email.message import EmailMessage
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime,timedelta
